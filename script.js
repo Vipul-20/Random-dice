8ed580c1cf36c698d5f111514c9e3bc6412afb5d
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-09-19 14:32:52 +0000

    javascript done

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log("hii");
+const els = document.querySelector("img");
+const btnEl = document.querySelector("button");
+console.log(els);
+
+function getRandom6() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+function getRandomUrl() {
+  const numWord = ["one", "two", "three", "four", "five", "six"];
+  const rn = getRandom6() - 1;
+  console.log(numWord[rn]);
+
+  const url = `assest/image/dice-${numWord[rn]}.png`;
+  return url;
+}
+// els.src = getRandomUrl();
+btnEl.addEventListener("click", function () {
+  els.src = getRandomUrl();
+});
